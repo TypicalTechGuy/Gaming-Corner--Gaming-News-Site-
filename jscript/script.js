70aa8031231f0412e911b398d0bb3031f66d4a7e
Fikhri Maheswara Taswandi <mummytix14@gmail.com>
2024-09-29 18:07:02 +0700

    Added "You may like" section below article, make the card looks tidier, updated css and jscript

--- jscript/script.js
+++ jscript/script.js
@@ -1,3 +1,28 @@
+function truncateText(text, maxLength) {
+    if (text.length <= maxLength) {
+      return text;
+    } else {
+      return text.slice(0, maxLength) + "...";
+    }
+  }
+  
+  document.querySelectorAll('.article-content h2').forEach((desc) => {
+    desc.textContent = truncateText(desc.textContent, 130);
+  });
+
+  document.querySelectorAll('.article-content p').forEach((desc) => {
+    desc.textContent = truncateText(desc.textContent, 200);
+  });
+
+  document.querySelectorAll('.related-item h3').forEach((title) => {
+    title.textContent = truncateText(title.textContent, 60);
+  });
+  
+  document.querySelectorAll('.related-item p').forEach((desc) => {
+    desc.textContent = truncateText(desc.textContent, 100);
+  });
+  
+
 document.querySelector('.menu-toggle').addEventListener('click', function() {
     document.querySelector('.nav-links').classList.toggle('active');
 });
